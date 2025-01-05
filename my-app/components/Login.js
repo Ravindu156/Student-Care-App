7cc8d33c5b8f8174876ef133efaf52e5f9af71da
Ravindu156 <puresoftlk@gmail.com>
2025-01-05 14:49:47 +0530

    Enhance Login component: add header, logo, input fields, and login button with improved styling

--- my-app/components/Login.js
+++ my-app/components/Login.js
@@ -1,13 +1,38 @@
-import { PaperProvider} from "react-native-paper";
-import { Text ,ScrollView,View} from "react-native";
+import { PaperProvider,Text ,Divider,Button,TextInput} from "react-native-paper";
+import {  ScrollView, View, StyleSheet,Image } from "react-native";
+
 export default function Login(){
     return(
         <PaperProvider>
           <ScrollView contentContainerStyle={styles.scrollView}>
                 <View style={styles.container}>
-                    <Text>
-                        HIIII
-                    </Text>
+                  <View style={styles.header}>
+                        <Text variant="headlineMedium">Uov Student Care</Text>
+
+                  </View>
+                  <View style={styles.imagepad}>
+                    <Image source={require('../assets/uovlogo.png')} style={styles.image}/>
+
+                  </View>
+                <View style={styles.banner}>
+                    <Text variant="headlineMedium">Student Login</Text>
+
+                </View>
+
+                <View style={styles.input}>
+                    <TextInput label="username" mode="outlined" style={styles.inputText}/>
+
+                </View>
+                <View style={styles.input}>
+                    <TextInput label="password" mode="outlined" style={styles.inputText}/>
+                </View>
+
+                <View style={styles.input}>
+                     <Button mode='contained'  style={styles.button}>
+                     <Text style={styles.buttonText}>Login</Text></Button>
+                </View>
+
+
 
 
                 </View>
@@ -47,6 +72,53 @@ const styles=StyleSheet.create({
     scrollView: {
         flexGrow: 1, // Ensures the ScrollView behaves correctly
     },
+    container:{
+        flexDirection: 'column',
+        backgroundColor: '#fff',
+        justifyContent: 'space-between',
+        padding: 0,
+    },
+    header:{
+        backgroundColor: '#500073',
+        justifyContent: 'space-between',
+        alignItems:'center',
+        height:100,
+        
+       
+    },
+    imagepad:{
+        padding: 8,
+        alignItems: 'center',
+       flex: 2,
+       marginBottom: 10
+    },
+    image:{
+       width:'80%',
+        height:70,
+    },
+    banner:{
+        alignItems:'center',
+        height:100,
+        
+    },
+    input:{
+        backgroundColor:'white',
+    },
+    inputText:{
+        backgroundColor:'white',
+        width:'80%',
+        
+        marginLeft:40,
+    },button:{
+        backgroundColor:'#500073',
+        width:'80%',
+        marginTop:40,
+        marginLeft:40,
+      
+    },
+    buttonText:{
+        color:'white',
+    }
 
 
 })
