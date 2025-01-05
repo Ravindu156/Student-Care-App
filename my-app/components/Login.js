d2211ea5b2dee3b96815ea3b25ee7211102893b4
Ravindu156 <puresoftlk@gmail.com>
2025-01-05 12:35:14 +0530

    Add Login component and integrate react-native-paper for UI

---
+++ my-app/components/Login.js
@@ -0,0 +1,45 @@
+import { PaperProvider ,Text} from "react-native-paper";
+
+export default function Login(){
+    return(
+        <PaperProvider>
+            <Text>
+                Hi
+
+
+            </Text>
+
+
+
+
+
+        </PaperProvider>
+
+
+
+
+
+    );
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
